d8be8e4b0c21b6c4a2ddd77a4b57ba2560d0500c
bsupinski <66806979+bsupinski@users.noreply.github.com>
2020-06-11 19:58:31 -0400

    Add files via upload

---
+++ project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,41 @@
+// variables for color selection and table
+let color = document.getElementById('colorPicker');
+let table = document.getElementById('pixelCanvas');
+let sizePicker = document.getElementById('sizePicker');
+// variables for grid size
+let height = document.getElementById('inputHeight').value;
+let width = document.getElementById('inputWidth').value;
+
+makeGrid(height, width);
+// event listener to remove auto refresh on submit button.
+sizePicker.addEventListener('submit', function() {
+  event.preventDefault();
+// information to determine grid size
+  let height = document.getElementById('inputHeight').value;
+  let width = document.getElementById('inputWidth').value;
+  //this remove the
+  table.firstElementChild.remove();
+
+  makeGrid(height, width);
+});
+
+// function to create the grid
+function makeGrid(height, width) {
+
+  for(i = 0; i < height; i++){
+    let row = table.insertRow(i);
+    for(j =0; j < width; j++){
+      let cell = row.insertCell(j);
+      //add style of color picker to grid selection
+      cell.addEventListener('click', function() {
+        if(cell.style.backgroundColor = color.value === color.value){
+          cell.style.backgroundColor = " ";
+        }
+        else{
+          cell.style.backgroundColor = color.value;
+
+        }
+      });
+    }
+  }
+}
